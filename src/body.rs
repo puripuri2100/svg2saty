30525ff6908fa548b201ecfe599485e787c92224
puripuri2100 <puripuri2100@gmail.com>
2020-05-07 00:04:00 +0900

    update

--- src/body.rs
+++ src/body.rs
@@ -13,39 +13,30 @@ pub fn main(svg: svg::parser::Parser) -> String {
   let mut main_str = String::new();
   for event in svg {
     match event {
-      Event::Tag(Path, _, attributes) => {
-        let s_opt = path::main(attributes);
-        match s_opt {
-          Some(s) => main_str.push_str(&format!("  {};\n", s)),
-          None => {}
+      Event::Tag(tag_name, _, attributes) => match tag_name {
+        "svg" => {
+          let f_len_opt = svg_tag::get_y_len(&attributes);
+          match f_len_opt {
+            Some(f) => y_len = f,
+            None => {}
+          }
         }
-      }
-      /*
-            Event::Tag(Pattern, _, attributes) => {}
-            Event::Tag(Polygon, _, attributes) => {}
-            Event::Tag(Polyline, _, attributes) => {}
-            Event::Tag(RadialGradient, _, attributes) => {}
-            Event::Tag(Script, _, attributes) => {}
-            Event::Tag(Stop, _, attributes) => {}
-            Event::Tag(Style, _, attributes) => {}
-            Event::Tag(Symbol, _, attributes) => {}
-            Event::Tag(Text, _, attributes) => {}
-            Event::Tag(TextPath, _, attributes) => {}
-            Event::Tag(Title, _, attributes) => {}
-            Event::Tag(Use, _, attributes) => {}
-      */
-      Event::Tag(_, _, attributes) => {
-        let f_len_opt = svg_tag::get_y_len(&attributes);
-        match f_len_opt {
-          Some(f) => y_len = f,
-          None => {}
+        "path" => {
+          let s_opt = path::main(attributes);
+          match s_opt {
+            Some(s) => main_str.push_str(&format!("  {};\n", s)),
+            None => {}
+          }
         }
-        let circle_s_opt = circle::main(&attributes);
-        match circle_s_opt {
-          Some(s) => main_str.push_str(&format!("  {};\n", s)),
-          None => {}
+        "circle" => {
+          let circle_s_opt = circle::main(&attributes);
+          match circle_s_opt {
+            Some(s) => main_str.push_str(&format!("  {};\n", s)),
+            None => {}
+          }
         }
-      }
+        _ => {}
+      },
       _ => {}
     }
   }
